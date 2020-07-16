b827c5bf5882d3de466947a137f8e87a7b0a8716
prabhakar.pandey <vky535kolkata@gmail.com>
2020-07-16 09:43:01 +0530

    added trip details in list vehicle call

--- models/vehicle-management/index.js
+++ models/vehicle-management/index.js
@@ -2,7 +2,19 @@ const _ = require('lodash'),
 Sequelize = require('sequelize');
 const functions = require('./../../functions'),
 	sequelize = functions.sequelize;
-	promisify = functions.promisify.promisify;
+    promisify = functions.promisify.promisify;
+    
+function sortObject(obj,keyName){
+    return obj.sort((a, b) => parseFloat(b[keyName]) - parseFloat(a[keyName]));
+}
+
+function getTripObject(trips){
+    const obj = sortObject(trips,"mobilityTrackerId")
+    return {
+        tripCount:trips.length,
+        lastTrip:obj[0]
+    }
+}
 	
 const get = {
 	
@@ -14,7 +26,29 @@ const get = {
             queryTag = `select * from vehicles where status = 0`;
         }
 		try {
-			return sequelize.query(queryTag, {type: Sequelize.QueryTypes.SELECT });
+            var vehicles = await sequelize.query(queryTag, {type: Sequelize.QueryTypes.SELECT });
+            var vehicleArray = [];
+            vehicles.forEach(element => {
+                vehicleArray.push(element.vehicleId)
+            });
+            const tripQuery = `select * from tripTrackers where VehicleId IN (${vehicleArray.join(",")})`;
+            var tripData = await sequelize.query(tripQuery, {type: Sequelize.QueryTypes.SELECT });
+            var tripObj = {};
+            tripData.forEach(trip=>{
+                if(tripObj[trip.VehicleId]){
+                    tripObj[trip.VehicleId].push(trip);
+                }else{
+                    tripObj[trip.VehicleId] = [trip]
+                }
+            })
+            const tripMapping = {};
+            for(trips in tripObj){
+                tripMapping[trips] = getTripObject(tripObj[trips])
+            }
+			return {
+                vehicles,
+                tripMapping
+            };
 		} catch (ex) {
 			return promisify(ex, '')
 		}
@@ -32,12 +66,19 @@ const get = {
             var queryTag = `select * from vehicles having registrationNumber = '${String(registrationNumber)}'`;
            
             const vehiclesData = await sequelize.query(queryTag, {type: Sequelize.QueryTypes.SELECT });
-            console.log("vehiclesData",vehiclesData)
             const resposeData = vehiclesData[0];
-            var tripQuery = `select * from tripTrackers where VehicleId = ${resposeData.vehicleId}`;
-            
-            resposeData.tripDetails = await sequelize.query(tripQuery, {type: Sequelize.QueryTypes.SELECT });
-            console.log("queryTag===>",resposeData)
+            var tripQuery = `select * from tripTrackers where VehicleId = ${resposeData.vehicleId} ORDER BY updatedAt DESC`;
+            var tripDetails = await sequelize.query(tripQuery, {type: Sequelize.QueryTypes.SELECT });
+            resposeData.tripDetails = {
+                tripInfo:tripDetails[0],
+                tripCount:tripDetails.length
+            }
+            var vehicleInfoQuery = `SELECT *
+                                    FROM (((vehiclePermits
+                                    INNER JOIN vehicleInsurances ON vehiclePermits.VehicleId = vehicleInsurances.VehicleId)
+                                    INNER JOIN vehicleFitnesses ON vehiclePermits.VehicleId = vehicleFitnesses.VehicleId)
+                                    INNER JOIN vehicleFeatures ON vehiclePermits.VehicleId = vehicleFeatures.VehicleId);`
+            resposeData.vehicleInfo = await sequelize.query(vehicleInfoQuery, {type: Sequelize.QueryTypes.SELECT });
 			return resposeData
 		} catch (ex) {
 			return promisify(ex, '')
